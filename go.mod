ed4db8c71a6d9ec6d9be4421f7a9f4faf7b9144b
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-24 17:51:23 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yesilyurtburak/go-web-database
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func main() {
+	// Connect to the database
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=blog_db user=postgres password=postgres")
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("Couldn't connect to the database : %v\n", err))
+	}
+	defer conn.Close()
+
+	// ping the database. check if it response.
+	err = conn.Ping()
+	if err != nil {
+		log.Fatalf("Couldn't ping the database : %v\n", err)
+	}
+}
