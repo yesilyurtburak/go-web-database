bed6590d566ebe859b10a96c944f9b5761d148eb
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-24 18:17:47 +0300

    added a function to retrieve data from database

--- main.go
+++ main.go
@@ -9,6 +9,32 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+// This function takes connection as parameter and returns a possible error.
+func getAllRowData(conn *sql.DB) error {
+	rows, err := conn.Query("SELECT id, name, email FROM users") // sql query
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer rows.Close() // close after it has done.
+
+	// create variables to hold the scanned data.
+	var id int
+	var name, email string
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name, &email) // scan the rows in the table and copy the values into vars.
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println("Data : ", id, name, email)
+	}
+	if err != nil {
+		log.Fatal("Error reading data : ", err)
+	}
+	return nil
+}
+
 func main() {
 	// Connect to the database
 	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=blog_db user=postgres password=postgres")
@@ -22,4 +48,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't ping the database : %v\n", err)
 	}
+
+	// function call for fetching data from database
+	err = getAllRowData(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("-------------------------")
 }
